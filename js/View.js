f4c0980dec309d708a7e0dee2047d549a2f910f7
Alexandra Biedermann <nina@net2000.ch>
2021-03-24 17:20:14 +0100

    Ajout des produits sur la page d'accueil.

--- js/View.js
+++ js/View.js
@@ -7,13 +7,31 @@ class View {
     showListProduct(listProduct) {
   
         let container= document.getElementById("container");    
-        for (let i=0; i<=listProduct.length; i++){
-        let viewProduct=listProduct[i];
-        viewProduct= document.createElement("article");
-        container.appendChild(viewProduct);
-        viewProduct.setAttribute ("id","produit"+i);
-
+        for (let i=0; i<listProduct.length; i++){
+            let viewProduct=listProduct[i];
+            let viewImage=listProduct[i];
+            viewProduct= document.createElement("article");
+            viewImage=document.createElement("div");
+            container.appendChild(viewProduct);
+            viewProduct.setAttribute ("id","produit"+i);
+            viewProduct.appendChild(viewImage);
+            viewImage.setAttribute("id","img"+i);
         }
+
+        for (let i=0; i<listProduct.length; i++){
+            let srcImage=listProduct[i].imageUrl;
+            let nom=listProduct[i].name;
+            let imageProduit= document.createElement("img");
+            let nomProduit= document.createElement("h2");
+            let img=document.getElementById("img"+i);
+            let produit=document.getElementById("produit"+i);
+            img.appendChild(imageProduit);
+            imageProduit.setAttribute("src", srcImage);
+            produit.appendChild(nomProduit);
+            nomProduit.innerHTML= nom;
+        }
+
+        
 
         
     }
