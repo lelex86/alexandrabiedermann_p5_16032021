9aa155f9645056e01924f08c4343182d3c1bd520
Alexandra Biedermann <nina@net2000.ch>
2021-03-25 21:28:22 +0100

    Finalisation page d'accueil + page produit (hors panier)

--- js/Controller.js
+++ js/Controller.js
@@ -13,26 +13,22 @@ class Controller {
 
     //Classe pour un produit en particulier en utilisant l'id à ajouter à notre url de base
     async showDetailProduct() {
-        let searchParams = new URLSearchParams(window.location.search);
-        let id = searchParams.get('id');
+        let id = new URLSearchParams(window.location.search).get('id');
         let url = 'http://localhost:3000/api/teddies/' + id;
-        let detailProduct = Model.get(url)
+        let detailProduct = await Model.get(url).then(function (response){
+            return (JSON.parse(response))
+        });
         let view = new View();
         view.showDetailProduct(detailProduct);
     }
 
     //Classe permettant d'afficher notre page panier
     async showPanier() {
-        let view = new View();
-        view.buyProduct(productBought);
+       
     }
 
     // page finale utilisant un POST pour afficher les données envoyées au server (Formulaire et produits)
     async postOrder() {
-        let postOrder = Model.post('http://localhost:3000/api/teddies/');
-        let view = new View();
-        view.cameraOrder(postOrder);
-        // la ligne suivante vide le panier apres chaque achats.
-        localStorage.clear();
+        
     }
 }
--- js/View.js
+++ js/View.js
@@ -8,11 +8,15 @@ class View {
   
         let container= document.getElementById("container");    
         for (let i=0; i<listProduct.length; i++){
+            let linkProduct=listProduct[i];
             let viewProduct=listProduct[i];
             let viewImage=listProduct[i];
+            linkProduct= document.createElement("a");
             viewProduct= document.createElement("article");
             viewImage=document.createElement("div");
-            container.appendChild(viewProduct);
+            container.appendChild(linkProduct);
+            linkProduct.setAttribute("href", "ficheProduit.html?id=" + listProduct[i]._id)
+            linkProduct.appendChild(viewProduct);
             viewProduct.setAttribute ("id","produit"+i);
             viewProduct.appendChild(viewImage);
             viewImage.setAttribute("id","img"+i);
@@ -40,6 +44,54 @@ class View {
 
     // On affiche le produit récuperé avec l'Id + l'URL via notre controller
     showDetailProduct(detailProduct) {
+
+        let nom = detailProduct.name;
+        let description = detailProduct.description;
+        let srcImage = detailProduct.imageUrl;
+        let prix= detailProduct.price/100;
+        let personalisation = detailProduct.colors;
+        let container= document.getElementById("container"); 
+        let imageProduit=document.createElement("img");
+        let nomProduit= document.createElement("h1");
+        let descriptionProduit = document.createElement("p");
+        let prixProduit = document.createElement("p")
+        let section=document.createElement("section");
+        let blocDroit=document.createElement("div");
+        let blocGauche=document.createElement("div");
+        let choixPersonalisation= document.createElement("select")
+        blocDroit.setAttribute("id", "droit");
+        blocGauche.setAttribute("id", "gauche");
+        imageProduit.setAttribute("src", srcImage);
+        container.appendChild(nomProduit);
+        container.appendChild(section);
+        section.appendChild(blocGauche);
+        section.appendChild(blocDroit);
+        blocGauche.appendChild(imageProduit);
+        blocDroit.appendChild(descriptionProduit);
+        blocDroit.appendChild(prixProduit);
+        nomProduit.innerHTML = nom;
+        descriptionProduit.innerHTML = description;
+        prixProduit.innerHTML = prix + "€";
+        blocDroit.appendChild(choixPersonalisation);
+        let bouton= document.createElement("button");
+        blocDroit.appendChild(bouton);
+        bouton.setAttribute("type", "submit");
+        bouton.innerHTML = "Ajouter au panier";
+        let optionDefault=document.createElement("option");
+        choixPersonalisation.appendChild(optionDefault);
+        optionDefault.innerHTML = "Veuillez séléctionner une personalisation";
+        
+
+        for (let i=0; i<detailProduct.colors.length; i++){
+            let option=detailProduct.colors[i];
+            option=document.createElement("option");
+            option.setAttribute("value",detailProduct.colors[i]);
+            option.innerHTML = detailProduct.colors[i];
+            choixPersonalisation.appendChild(option);
+        }
+
+
+
                 
     }
  
