2f9408fabcd5102f7626790d72de2ef4c2d43fa6
Alexandra Biedermann <nina@net2000.ch>
2021-04-29 22:37:44 +0200

    Amélioration de la mise en page du code.

--- js/Controller.js
+++ js/Controller.js
@@ -1,50 +1,51 @@
-let panier= JSON.parse(localStorage.getItem("panier"));
-let id = new URLSearchParams(window.location.search).get('id');
-let url="http://localhost:3000/api/teddies/";
-let urlId=url + id;
-let urlOrder=url+"/order";
+let panier = JSON.parse(localStorage.getItem("panier"));
+let id = new URLSearchParams(window.location.search).get("id");
+let url = "http://localhost:3000/api/teddies/";
+let urlId = url + id;
+let urlOrder = url + "/order";
 class Controller {
-    //Fonctions générales
-    static getFormData(formData) {
-        let objectData = {};
-        formData.forEach(function (value, key) {
-            objectData[key] = value;
-        });
-        return (objectData);
+  //Fonctions générales
+  static getFormData(formData) {
+    let objectData = {};
+    formData.forEach(function (value, key) {
+      objectData[key] = value;
+    });
+    return objectData;
+  }
+  static displayBasketCount() {
+    if (localStorage.getItem("panier") == null) {
+      let panierInitialisation = [];
+      localStorage.setItem("panier", JSON.stringify(panierInitialisation));
     }
-    static displayBasketCount(){
-        if (localStorage.getItem("panier")== null){
-            let panierInitialisation=[];
-            localStorage.setItem("panier", JSON.stringify(panierInitialisation));
-        }
-        document.getElementById("nav").innerHTML= /*html*/`
+    document.getElementById("nav").innerHTML = /*html*/ `
             <a href="panier.html" rel=”nofollow” aria-label="liens vers le panier">
                 <i class="fas fa-shopping-cart"></i>
-                <p>${JSON.parse(window.localStorage.getItem("panier")).length}</p>
+                <p>${
+                  JSON.parse(window.localStorage.getItem("panier")).length
+                }</p>
             </a>
             `;
-    }
-    static addToBasket(){
-        let product =Model.get(urlId)
-        .then(function (response){
-            console.log("Connexion à l'API réussie!");
-            product=JSON.parse(response);
-            product.uniqueId = new Date().getTime();
-            panier.push(product);
-            console.log("panier", panier);
-            localStorage.setItem("panier", JSON.stringify(panier));
-            Controller.displayBasketCount();
-            Controller.confirmBox();
-        })
-        .catch(function (error){
-            console.error("Échec connexion API. Erreur=", error);
-            Controller.displayBasketCount();
-            Controller.errorBox();
-            
-        });
-    }
-    static confirmBox(){
-        document.getElementById("container").innerHTML += /*html*/`
+  }
+  static addToBasket() {
+    let product = Model.get(urlId)
+      .then(function (response) {
+        console.log("Connexion à l'API réussie!");
+        product = JSON.parse(response);
+        product.uniqueId = new Date().getTime();
+        panier.push(product);
+        console.log("panier", panier);
+        localStorage.setItem("panier", JSON.stringify(panier));
+        Controller.displayBasketCount();
+        Controller.confirmBox();
+      })
+      .catch(function (error) {
+        console.error("Échec connexion API. Erreur=", error);
+        Controller.displayBasketCount();
+        Controller.errorBox();
+      });
+  }
+  static confirmBox() {
+    document.getElementById("container").innerHTML += /*html*/ `
             <div id="customBoxContainer">
                 <div class="custom-box">
                     <p>L'article a bien été ajouté au panier</p>
@@ -55,9 +56,9 @@ class Controller {
                 </div>
             </div>
         `;
-    }
-    static errorBox(){
-        document.getElementById("container").innerHTML += /*html*/`
+  }
+  static errorBox() {
+    document.getElementById("container").innerHTML += /*html*/ `
             <div id="customBoxContainer">
                 <div class="custom-box">
                     <p>Nous avons rencontré un problème technique, l’article n’a pas pu être ajouté à votre panier. Veuillez nous excuser du désagrément et réessayer.</p>
@@ -67,105 +68,103 @@ class Controller {
                 </div>
             </div>
         `;
+  }
+  static deleteBasketLine(basketLine) {
+    panier = JSON.parse(localStorage.getItem("panier"));
+    // Supprime dans le panier l'élément donc le champ uniqueId correspond à celui du nounours supprimé
+    let newPanier = panier.filter(function (element) {
+      return element.uniqueId != basketLine.id;
+    });
+    //-------------------------------------------------------
+    localStorage.setItem("panier", JSON.stringify(newPanier));
+    basketLine.parentNode.remove();
+    if (newPanier.length == 0) {
+      window.location.replace("index.html");
+    } else {
+      Controller.displayTotalPrice();
     }
-    static deleteBasketLine(basketLine) {
-        panier= JSON.parse(localStorage.getItem("panier"));
-        // Supprime dans le panier l'élément donc le champ uniqueId correspond à celui du nounours supprimé
-        let newPanier = panier.filter(function (element) {
-            return (element.uniqueId != basketLine.id);
-        });
-        //-------------------------------------------------------
-        localStorage.setItem("panier", JSON.stringify(newPanier));
-        basketLine.parentNode.remove();
-        if (newPanier.length==0){
-            window.location.replace("index.html");
-        } else {
-            Controller.displayTotalPrice();
-        }
+  }
+  static displayTotalPrice() {
+    let totalPrice = 0;
+    let productsBought = JSON.parse(localStorage.getItem("panier"));
+    for (let productBought of productsBought) {
+      totalPrice += productBought.price / 100;
     }
-    static displayTotalPrice(){
-        let totalPrice=0;
-        let productsBought=JSON.parse(localStorage.getItem("panier"));
-        for (let productBought of productsBought){  
-          totalPrice+=productBought.price/100;
-        }
-        document.getElementById("totalPrice").innerHTML=totalPrice + "€";
-        sessionStorage.setItem("totalPrice", JSON.stringify(totalPrice));
+    document.getElementById("totalPrice").innerHTML = totalPrice + "€";
+    sessionStorage.setItem("totalPrice", JSON.stringify(totalPrice));
+  }
+  static sendOrder() {
+    let formulaire = document.getElementById("formulaire");
+    let data = new FormData(formulaire);
+    let contact = Controller.getFormData(data);
+    let products = [];
+    for (let i = 0; i < panier.length; i++) {
+      products.push(panier[i]._id);
     }
-    static sendOrder(){
-        let formulaire= document.getElementById("formulaire");
-        let data=new FormData(formulaire);
-        let contact=Controller.getFormData(data);
-        let products=[];
-        for (let i=0; i< panier.length; i++){
-            products.push(panier[i]._id)
-        };
-        let order=JSON.stringify({contact,products});
-        Model.post(urlOrder, order)
-        .then(function (response){
-            console.log("Connexion à l'API réussie!");
-            sessionStorage.setItem("commandNumber", JSON.parse(response).orderId);
-            localStorage.clear();
-            window.location.replace("commande.html");
-        })
-        .catch(function (error){
-            console.log("Échec connexion API. Erreur=", error);
-            window.location.replace("commande.html");
-        });  
-    }
-    
-    //Classe à appeler dans la page html pour afficher la page
-    
-    //classe pour la page d'accueil
-    showListProduct() {
-        let listProduct = Model.get(url)
-        .then(function (response){
-            console.log("Connexion à l'API réussie!");
-            let view = new View();
-            view.showListProduct(JSON.parse(response)); 
-        })
-        .catch(function (error){
-            console.error("Échec connexion API. Erreur=", error);
-            let view = new View();
-            view.showListProduct([]);
-        });
-    }
+    let order = JSON.stringify({ contact, products });
+    Model.post(urlOrder, order)
+      .then(function (response) {
+        console.log("Connexion à l'API réussie!");
+        sessionStorage.setItem("commandNumber", JSON.parse(response).orderId);
+        localStorage.clear();
+        window.location.replace("commande.html");
+      })
+      .catch(function (error) {
+        console.log("Échec connexion API. Erreur=", error);
+        window.location.replace("commande.html");
+      });
+  }
 
-    //Classe pour un produit en particulier en utilisant l'id à ajouter à notre url de base
-    showDetailProduct() {
-        let detailProduct =Model.get(urlId)
-        .then(function (response){
-            console.log("Connexion à l'API réussie!");
-            let view = new View();
-            view.showDetailProduct(JSON.parse(response));
+  //Classe à appeler dans la page html pour afficher la page
 
-        })
-        .catch(function (error){
-            console.error("Échec connexion API. Erreur=", error);
-            let view = new View();
-            view.showDetailProduct([]);
-        });
-    }
-
-    //Classe permettant d'afficher notre page panier
-   showPanier() {
-        if(panier.length>0){
-            let productsBought=JSON.parse(localStorage.getItem("panier"));
-            let view = new View();
-            view.buyProduct(productsBought);
-        }
-        else{
-            let view = new View();
-            view.errorBasket();
-        } 
-    }
-    
-    // page finale utilisant un POST pour afficher les données envoyées au server (Formulaire et produits)
-    postOrder() {
-        let commandNumber=window.sessionStorage.getItem("commandNumber");
-        let totalPrice=JSON.parse(window.sessionStorage.getItem("totalPrice"));
-        let postOrder=[commandNumber, totalPrice];
+  //classe pour la page d'accueil
+  showListProduct() {
+    let listProduct = Model.get(url)
+      .then(function (response) {
+        console.log("Connexion à l'API réussie!");
         let view = new View();
-        view.productOrder(postOrder);
+        view.showListProduct(JSON.parse(response));
+      })
+      .catch(function (error) {
+        console.error("Échec connexion API. Erreur=", error);
+        let view = new View();
+        view.showListProduct([]);
+      });
+  }
+
+  //Classe pour un produit en particulier en utilisant l'id à ajouter à notre url de base
+  showDetailProduct() {
+    let detailProduct = Model.get(urlId)
+      .then(function (response) {
+        console.log("Connexion à l'API réussie!");
+        let view = new View();
+        view.showDetailProduct(JSON.parse(response));
+      })
+      .catch(function (error) {
+        console.error("Échec connexion API. Erreur=", error);
+        let view = new View();
+        view.showDetailProduct([]);
+      });
+  }
+
+  //Classe permettant d'afficher notre page panier
+  showPanier() {
+    if (panier.length > 0) {
+      let productsBought = JSON.parse(localStorage.getItem("panier"));
+      let view = new View();
+      view.buyProduct(productsBought);
+    } else {
+      let view = new View();
+      view.errorBasket();
     }
+  }
+
+  // page finale utilisant un POST pour afficher les données envoyées au server (Formulaire et produits)
+  postOrder() {
+    let commandNumber = window.sessionStorage.getItem("commandNumber");
+    let totalPrice = JSON.parse(window.sessionStorage.getItem("totalPrice"));
+    let postOrder = [commandNumber, totalPrice];
+    let view = new View();
+    view.productOrder(postOrder);
+  }
 }
--- js/View.js
+++ js/View.js
@@ -1,12 +1,11 @@
 class View {
+  /*******************************PAGE D'ACCEUIL ********************************/
 
-/*******************************PAGE D'ACCEUIL ********************************/
-
-    showListProduct(listProduct) {
-        Controller.displayBasketCount();
-        if(listProduct.length>0){
-            for (let i=0; i<listProduct.length; i++){
-                document.getElementById("container").innerHTML+=/*html*/`
+  showListProduct(listProduct) {
+    Controller.displayBasketCount();
+    if (listProduct.length > 0) {
+      for (let i = 0; i < listProduct.length; i++) {
+        document.getElementById("container").innerHTML += /*html*/ `
                     <a href= "fiche-produit.html?id=${listProduct[i]._id}" aria-label="lien vers le produit">
                         <article>
                             <div>
@@ -15,29 +14,33 @@ class View {
                             </div>
                         </article>
                     </a>`;
-            }
-        } else {
-            document.getElementById("container").innerHTML=/*html*/`
+      }
+    } else {
+      document.getElementById("container").innerHTML = /*html*/ `
             <article class="error"> 
                 <h2>Problème technique</h2>
                 <p><i class="fas fa-exclamation-triangle"></i></p>
                 <p> En raison d’un problème technique nous ne sommes pas en mesure de vous présenter nos produits.</br>
                 Veuillez nous excuser de la gêne occasionnée.</p>
             </article>
-            `;   
-        }
+            `;
     }
+  }
 
-    /***********************PAGE ARTICLE INDIVIDUEL **************************/
+  /***********************PAGE ARTICLE INDIVIDUEL **************************/
 
-    // On affiche le produit récuperé avec l'Id + l'URL via notre controller
-    showDetailProduct(detailProduct) {
-        Controller.displayBasketCount();     
-        if(detailProduct.length==0){
-            document.querySelector('meta[name="description"]').setAttribute("content", "Ceci est une page d'erreur.");
-            document.querySelector('meta[property="og:description"]').setAttribute("content", "Ceci est une page d'erreur.");
-            document.querySelector("title").innerHTML="erreur";
-            document.getElementById("container").innerHTML =/*html*/`
+  // On affiche le produit récuperé avec l'Id + l'URL via notre controller
+  showDetailProduct(detailProduct) {
+    Controller.displayBasketCount();
+    if (detailProduct.length == 0) {
+      document
+        .querySelector('meta[name="description"]')
+        .setAttribute("content", "Ceci est une page d'erreur.");
+      document
+        .querySelector('meta[property="og:description"]')
+        .setAttribute("content", "Ceci est une page d'erreur.");
+      document.querySelector("title").innerHTML = "erreur";
+      document.getElementById("container").innerHTML = /*html*/ `
             <h1>Problème technique</h1>
             <article class="error">
                 <p><i class="fas fa-exclamation-triangle"></i></p>
@@ -46,19 +49,35 @@ class View {
                 <button onclick="window.location.replace('index.html')">Retour à l'accueil</button>
             </article>
             `;
-        } else{
-            document.querySelector('meta[name="description"]').setAttribute("content", "Découvrez les particularités de "+detailProduct.name+", nounours d'exception.");
-            document.querySelector('meta[property="og:description"]').setAttribute("content", "Découvrez les particularités de "+detailProduct.name+", nounours d'exception.");
-            document.querySelector("title").innerHTML=detailProduct.name;
-            document.getElementById("container").innerHTML =/*html*/`
+    } else {
+      document
+        .querySelector('meta[name="description"]')
+        .setAttribute(
+          "content",
+          "Découvrez les particularités de " +
+            detailProduct.name +
+            ", nounours d'exception."
+        );
+      document
+        .querySelector('meta[property="og:description"]')
+        .setAttribute(
+          "content",
+          "Découvrez les particularités de " +
+            detailProduct.name +
+            ", nounours d'exception."
+        );
+      document.querySelector("title").innerHTML = detailProduct.name;
+      document.getElementById("container").innerHTML = /*html*/ `
                 <h1 class="product-name">${detailProduct.name}</h1>
                 <section class="detailProduct">
                     <div>
-                        <img src="${detailProduct.imageUrl}" alt="Nounours ${detailProduct.name}"/>
+                        <img src="${detailProduct.imageUrl}" alt="Nounours ${
+        detailProduct.name
+      }"/>
                     </div>
                     <div>
                         <p>${detailProduct.description}</p>
-                        <p>${detailProduct.price/100}€</p>
+                        <p>${detailProduct.price / 100}€</p>
                         <form onsubmit="Controller.addToBasket(this); return false;">
                             <select id="select" required>
                                 <option value="">Veuillez choisir une personalisation</option>
@@ -68,57 +87,58 @@ class View {
                         
                     </div>
                 </section>`;
-            for (let i=0; i<detailProduct.colors.length; i++){
-                document.getElementById("select").innerHTML +=/*html*/`
+      for (let i = 0; i < detailProduct.colors.length; i++) {
+        document.getElementById("select").innerHTML += /*html*/ `
                     <option value="${detailProduct.colors[i]}">${detailProduct.colors[i]}</option>`;
-            }
-            
-        }
+      }
     }
- 
-    /*******************************PAGE PANIER ********************************/
+  }
 
-    // Nous recupérons les données des produits achetés pour les afficher dans la page panier
-    buyProduct(productsBought) {
-        for (let productBought of productsBought){
-            document.getElementById("listProductBought").innerHTML +=/*html*/ `
+  /*******************************PAGE PANIER ********************************/
+
+  // Nous recupérons les données des produits achetés pour les afficher dans la page panier
+  buyProduct(productsBought) {
+    for (let productBought of productsBought) {
+      document.getElementById("listProductBought").innerHTML += /*html*/ `
             <article class="basket">
-                <div><img src= "${productBought.imageUrl}" alt="Nounours ${productBought.name}"/></div>
+                <div><img src= "${productBought.imageUrl}" alt="Nounours ${
+        productBought.name
+      }"/></div>
                 <h2 class="product-name">${productBought.name}</h2>
-                <p>${productBought.price/100}€</p>
-                <button id="${productBought.uniqueId}" onclick="Controller.deleteBasketLine(this);">Retirer du panier</button>
+                <p>${productBought.price / 100}€</p>
+                <button id="${
+                  productBought.uniqueId
+                }" onclick="Controller.deleteBasketLine(this);">Retirer du panier</button>
             </article>`;
-        } 
-        document.getElementById("listProductBought").innerHTML +=/*html*/`
+    }
+    document.getElementById("listProductBought").innerHTML += /*html*/ `
         <article class="basket">
             <h2>Total du panier : </h2>
             <p id="totalPrice"></p>
         </article>`;
-        Controller.displayTotalPrice();
-    }
-    errorBasket() {
-        document.getElementById("listProductBought").innerHTML +=/*html*/`
+    Controller.displayTotalPrice();
+  }
+  errorBasket() {
+    document.getElementById("listProductBought").innerHTML += /*html*/ `
         <article class="error">
             <h2>Panier vide!</h2>
             <p><i class="fas fa-times-circle"></i></p>
             <p> Il semblerait que vous ayez atteri ici par erreur. Séléctionnez au moins un de nos nounours avant de revenir ici.</p>
             <button onclick="window.location.replace('index.html')">Retourner à l'accueil</button>
         </article>`;
-        document.getElementById("form").style.display="none";
-        
-    }
+    document.getElementById("form").style.display = "none";
+  }
 
-    /***************************PAGE DE CONFIRMATION ***************************/
+  /***************************PAGE DE CONFIRMATION ***************************/
 
-    // Page finale avec les données que nous prenons du POST ainsi que le total de la commande
-    productOrder(postOrder) {
-        
-        let totalPrice=postOrder[1];
-        let commandNumber=postOrder[0];
-        if(commandNumber==null || commandNumber==undefined){
-            document.getElementById("orderText").innerHTML = 
-            "Problème lors de l'envoi de votre commande";
-            document.getElementById("command").innerHTML +=/*html*/`
+  // Page finale avec les données que nous prenons du POST ainsi que le total de la commande
+  productOrder(postOrder) {
+    let totalPrice = postOrder[1];
+    let commandNumber = postOrder[0];
+    if (commandNumber == null || commandNumber == undefined) {
+      document.getElementById("orderText").innerHTML =
+        "Problème lors de l'envoi de votre commande";
+      document.getElementById("command").innerHTML += /*html*/ `
                 <article class="error">
                     <p><i class="fas fa-exclamation-triangle"></i></p>
                     <p>Malheureusement l'envoi de votre commande a rencontré un problème technique.<br /> 
@@ -127,8 +147,8 @@ class View {
                     <button onclick="window.location.replace('panier.html')">Retourner au panier</button>
                 </article>
                 `;
-        } else{
-            document.getElementById("command").innerHTML +=/*html*/ `
+    } else {
+      document.getElementById("command").innerHTML += /*html*/ `
             <article>
                 <div class="commandConfirmation">
                     <p>Votre numéro de commande est le : </p>
@@ -137,7 +157,7 @@ class View {
                 <p id="totalPrice">Prix total de votre commande: ${totalPrice}€</p>
                 <button onclick="sessionStorage.clear(); window.location.replace('index.html')">Retour à la boutique</button>
             </article>
-        `; 
-        }   
+        `;
     }
+  }
 }
